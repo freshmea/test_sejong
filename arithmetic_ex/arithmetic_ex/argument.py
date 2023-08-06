074428bb44b7c6f07523a397189449e333938457
freshmea <fresmea@naver.com>
2023-08-05 23:52:13 -0700

    argument

--- arithmetic_ex/arithmetic_ex/argument.py
+++ arithmetic_ex/arithmetic_ex/argument.py
@@ -1,13 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from my_interfaces.msg import ArithmeticArgument
 
 class Argument(Node):
     def __init__(self):
-        super().__init__('msub')
-        self.create_subscription(String, 'message', self.sub, 10)
-    def sub(self, msg):
-        self.get_logger().info(f'Recived message: {msg.data}')
+        super().__init__('argument')
+        self.pub = self.create_publisher(ArithmeticArgument, 'arithmtic_argument', 10)
+        self.timer = self.create_timer(1, self.publisher)
+        
+    def publisher(self):
+        msg = ArithmeticArgument()
+        msg.stamp = self.get_clock().now().to_msg()
+        msg.argument_a = 3.0
+        msg.argument_b = 5.0
+        self.pub.publish(msg)
 
 def main():
     rclpy.init()
