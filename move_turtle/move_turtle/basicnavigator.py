f6bb4c0d4e6a4a0c45f20f8d67c78e5f3fa52929
freshmea <fresmea@naver.com>
2023-12-19 17:18:09 -0800

    aa

--- move_turtle/move_turtle/basicnavigator.py
+++ move_turtle/move_turtle/basicnavigator.py
@@ -12,15 +12,15 @@ class Gpio_led_server(Node):
     to a GPIO pin on a Raspberry Pi.
 
     """
-    
+
     def __init__(self):
         """
         Initializes the GPIO LED server node.
         This method sets up a ROS service that can be used to control an LED
         connected to GPIO pin 21 on a Raspberry Pi.
         """
-        super().__init__('gpioLedServer')
-        self.srv = self.create_service(SetBool, 'gpio_led_server', self.gpio_led)
+        super().__init__("gpioLedServer")  # type: ignore
+        self.srv = self.create_service(SetBool, "gpio_led_server", self.gpio_led)
         g.setmode(g.BCM)
         g.setup(21, g.OUT)
 
@@ -33,13 +33,13 @@ class Gpio_led_server(Node):
         Returns:
             _type_: _description_
         """
-        self.get_logger().info(f'incomming data{request.data}')
+        self.get_logger().info(f"incomming data{request.data}")
         if request.data:
             g.output(21, True)
         else:
             g.output(21, False)
         response.success = True
-        response.message = 'ok'
+        response.message = "ok"
         return response
 
 
@@ -59,5 +59,5 @@ def main(args=None):
         rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
